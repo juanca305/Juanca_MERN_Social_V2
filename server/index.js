eec866a036a00d642e2d0415a53b95c29e85fd9b
Juan Carlos <jc.rdguez64@gmail.com>
2024-10-01 13:59:21 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -36,6 +36,16 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
+//********************************
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin','*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH');
+  next();
+});
+//***********************************************
+
 //Setting a local directory for images storage
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
