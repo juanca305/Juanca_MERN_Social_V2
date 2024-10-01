3a667245a686e6d641e7a7eab494395f69da3c09
Juan Carlos Rodriguez <jc.rdguez64@gmail.com>
2024-10-01 06:29:31 -0400

    Last commit

---
+++ client/src/components/Friend.jsx
@@ -0,0 +1,94 @@
+import { PersonAddOutlined, PersonRemoveOutlined } from "@mui/icons-material";
+import { Box, IconButton, Typography, useTheme } from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { setFriends } from "state";
+import FlexBetween from "./FlexBetween";
+import UserImage from "./UserImage";
+import { useEffect } from "react";
+
+
+const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { _id } = useSelector((state) => state.user);
+  const token = useSelector((state) => state.token);
+  const friends = useSelector((state) => state.user.friends);
+
+  const { palette } = useTheme();
+  const primaryLight = palette.primary.light;
+  const primaryDark = palette.primary.dark;
+  const main = palette.neutral.main;
+  const medium = palette.neutral.medium;
+
+  const isFriend = friends.find((friend) => friend._id === friendId);
+  const isSelf = friendId === _id;
+  const isLoggedInFriend = friends.find((friend) => friend._id === _id);
+
+ 
+    const patchFriend = async () => {
+    const response = await fetch(
+      `https://juanca-mern-social-app.onrender.com/users/${_id}/${friendId}`,
+      {
+        method: "PATCH",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const data = await response.json();
+    console.log("DATA", data);
+    dispatch(setFriends({ friends: data }));
+    navigate(0);
+  };
+    
+  
+  return (
+    <FlexBetween>
+      <FlexBetween gap="1rem">
+        <UserImage image={userPicturePath} size="55px" />
+        <Box
+          onClick={() => {
+            navigate(`/profile/${friendId}`);
+            navigate(0);
+          }}
+        >
+          <Typography
+            color={main}
+            variant="h5"
+            fontWeight="500"
+            sx={{
+              "&:hover": {
+                color: palette.primary.light,
+                cursor: "pointer",
+              },
+            }}
+          >
+            {name}
+          </Typography>
+          <Typography color={medium} fontSize="0.75rem">
+            {subtitle}
+          </Typography>
+        </Box>
+      </FlexBetween>
+
+      {!isSelf && !isLoggedInFriend && (
+        <IconButton
+        onClick={() => patchFriend()}
+        sx={{ backgroundColor: primaryLight, p: "0.6rem" }}
+      >
+        {isFriend ? (
+          <PersonRemoveOutlined sx={{ color: primaryDark }} />
+        ) : (
+          <PersonAddOutlined sx={{ color: primaryDark }} />
+        )}
+      </IconButton>
+      )}
+      
+    </FlexBetween>
+
+  );
+};
+
+export default Friend;
---
+++ client/src/scenes/widgets/AdvertWidget.jsx
@@ -0,0 +1,41 @@
+import { Typography, useTheme } from "@mui/material";
+import FlexBetween from "components/FlexBetween";
+import WidgetWrapper from "components/WidgetWrapper";
+
+const AdvertWidget = () => {
+  const { palette } = useTheme();
+  const dark = palette.neutral.dark;
+  const main = palette.neutral.main;
+  const medium = palette.neutral.medium;
+
+  return (
+    <WidgetWrapper>
+      <FlexBetween>
+        <Typography color={dark} variant="h5" fontWeight="500">
+          Sponsored
+        </Typography>
+        <Typography color={medium}>Create Ad</Typography>
+      </FlexBetween>
+      <img
+        width="100%"
+        height="auto"
+        alt="advert"
+        src="https://juanca-mern-social-app.onrender.com/assets/beatles_guitars.jpg"
+        style={{ borderRadius: "0.75rem", margin: "0.75rem 0" }}
+      />
+      <FlexBetween>
+        <Typography color={main}>Beatles Guitars</Typography>
+        <Typography color={medium}>beatlesguitars.com</Typography>
+      </FlexBetween>
+      <Typography color={medium} m="0.5rem 0">
+        {/* Your pathway to stunning and immaculate beauty and made sure your skin
+        is exfoliating skin and shining like light. */}
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ducimus at
+        praesentium consequatur rem error repellendus? Labore ab libero sequi
+        vero!
+      </Typography>
+    </WidgetWrapper>
+  );
+};
+
+export default AdvertWidget;
